a8b4e85a6fc1ab9f0858b046b89a0bb5f5c28d74
lcylichenyi <289172988@qq.com>
2019-05-21 00:22:11 +0800

    router

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,29 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <el-button>el-button</el-button>
+    <div class="tab">
+      <router-link to="/">
+        <el-button type="success" round class="tabbtn">图片</el-button> 
+      </router-link>
+      <router-link to="/svg">
+        <el-button type="primary" round class="tabbtn">SVG</el-button>
+      </router-link>
     </div>
-    <Main/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Main from './components/Main.vue'
+import Home from './views/Home.vue'
 
 export default {
   name: 'app',
   components: {
-    Main,
+    Home,
   },
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -27,5 +31,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.tab {
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  a {
+    margin: 0 50px;
+    margin-bottom: 50px;
+  }
+  .tabbtn {
+    width: 150px;
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,74 @@
 <template>
-  <div class="home">
-      <!-- <button type="button" @click="getJson">从后端取json</button>
-      <div class="showJson">{{json}}</div> -->
-      <Main msg="" />
+  <div class="container">
+    <div class="imgContainer">
+      <!-- <div v-for="(i,index) in fakeImg" :key="index" :style="{backgroundImage:'url('+ i + ')'}" @click="clickImg($event)"></div> -->
+      <div v-for="(i,index) in fakeImg" :key="index">
+        <img :src="i" @click="clickImg($event)" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import store from '../store';
-import { Component, Vue } from 'vue-property-decorator';
-import Main from '@/components/Main.vue'; // @ is an alias to /src
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import copy from 'copy-string'
+import { Message } from 'element-ui';
+Vue.prototype.$message = Message
 
-@Component({
-  components: {
-    Main,
-  },
-})
-export default class Home extends Vue {
-  get json() {
-    return store.state.json;
+@Component
+export default class Main extends Vue {
+  // private url = 'http://localhost:3000/public/image/bg_image_upload.png'
+  private url = 'https://i0.hdslb.com/bfs/article/ae1e87fe7080bcb975c57a5073fca83148fee9d1.jpg@1320w_590h.webp'
+  private fakeImg = [this.url, this.url, this.url, this.url, this.url, this.url, this.url, this.url ]
+
+  @Prop() private msg!: string
+
+  private mounted() {
+    console.log(this.$message)
   }
-  public getJson() {
-    store.dispatch('getJson');
-   }
+  private clickImg(e: any) {
+    copy(e.srcElement.src, this.successMsg, this.errorMsg)
+  }
+  private errorMsg() {
+    this.$message.error('复制失败')
+  }
+
+  private successMsg() {
+    this.$message({
+          showClose: true,
+          message: '复制成功',
+          type: 'success',
+    })
+  }
 }
 </script>
 
-
-<style scoped>
-
+<style scoped lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
+.container {
+  .imgContainer {
+    margin: 0 auto;
+    width: 1000px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    div {
+      box-sizing: border-box;
+      border: 1px solid black;
+      width: 200px;
+      height: 200px;
+      overflow: hidden;
+      img {
+        &:hover {
+          cursor: pointer;
+        }
+        height: 150px;
+      }
+    }
+  }
+}
 </style>
